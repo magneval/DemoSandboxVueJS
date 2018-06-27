8fb816dd3d4d9c615e30931b19d5103de02e9cd2
Charles de MAGNEVAL <magneval@gmail.com>
2018-06-27 12:33:51 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    <hello-world/>
+  </div>
+</template>
+
+<script>
+import test from "./components/hello-world";
+
+export default {
+  name: "App",
+  components: {
+    "hello-world":test
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
